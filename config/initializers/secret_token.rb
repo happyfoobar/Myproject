ff7739677d6d34a0c2b9cf5e8615fd51a03d9efe
Foobar <seems@emailgo.de>
2011-03-15 14:42:00 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = 'b92f59029db667f694017dbe3a099a4c7207399befa74188d82e7ba5cc178621f5e950eb5cd744a3c09bf594923fb2b8fae36af064e5013c6aa10d048290a973'
